97309d238e55d0c726a4ce8ffa2e79a0f23118d6
puhuk <wetr235@gmail.com>
2019-07-27 22:15:49 +0900

    Updates

---
+++ precache-manifest.84deba893586d70e248ca2e6241057d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80dea950e64a35e6e392f60f19ba1e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5e4e05db49cead905d2",
+    "url": "/static/css/main.8c5089d2.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "c5e4e05db49cead905d2",
+    "url": "/static/js/main.96eee084.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
